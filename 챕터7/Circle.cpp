5d754d4168c2287de0b47c06b636c36146d3b865
JoonKyo Jung <54362230+gkeksd@users.noreply.github.com>
2024-07-19 13:18:56 +0900

    챕터7 Circle 연습

---
+++ 챕터7/Circle.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "Circle.h"
+using namespace std;
+
+Circle::Circle() : radius(0.0) {
+    cout << "기본 생성자가 호출되었습니다." << endl;
+}
+
+Circle::Circle(double rds) : radius(rds) {
+    cout << "매개변수가 생성자가 호출되었습니다." << endl;
+}
+
+Circle::Circle(const Circle& circle) : radius(circle.radius) {
+    cout << "복사 생성자가 호출되었습니다." << endl;
+}
+
+Circle::~Circle() {
+    cout << "소멸자가 호출되었습니다. 반지름: " << radius << endl;
+}
+
+void Circle::setRadius(double value) {
+    radius = value;
+}
+
+double Circle::getRadius() const {
+    return radius;
+}
+
+double Circle::getArea() const {
+    return PI * radius * radius;
+}
+
+double Circle::getPerimeter() const {
+    return 2 * PI * radius;
+}
