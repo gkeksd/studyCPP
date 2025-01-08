1712ecae2c8f8a86dce187a862e0848cb044555a
JoonKyo Jung <54362230+gkeksd@users.noreply.github.com>
2025-01-08 17:27:55 +0900

    헤더파일 분리용 수정

--- 챕터14/dynamicarray.h
+++ 챕터14/dynamicarray.h
@@ -7,19 +7,8 @@ class DynamicArray {
 	int* ptr;
 	int size;
 public:
-	DynamicArray(int size) : size(size) {
-		ptr = new int[size];
-	}
-	~DynamicArray() {
-		delete[] ptr;
-		ptr = nullptr;
-	}
-	void setAt(int index, int value) {
-		if (index < 0 || index >= size) throw MyException(4882, "ÇÒ´ç ¿À·ù\n", this);
-		ptr[index] = value;
-	}
-	int getAt(int index) {
-		if (index < 0 || index >= size) throw MyException(4882, "Á¢±Ù ¿À·ù\n", this);
-		else return ptr[index];
-	}
+	DynamicArray(int size);
+	virtual ~DynamicArray();
+	void setAt(int index, int value);
+	int getAt(int index);
 };
