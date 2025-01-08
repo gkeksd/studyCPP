d0be3518e4f9b9b681c008a62d0fc6a0f2393466
JoonKyo Jung <54362230+gkeksd@users.noreply.github.com>
2025-01-08 17:58:39 +0900

    주석 추가

--- 챕터14/예외처리_02.cpp
+++ 챕터14/예외처리_02.cpp
@@ -10,7 +10,7 @@ int main() {
 		cout << "또다른 정수를 입력하세요: ";
 		cin >> num2;
 
-		//패턴 1. try-catch 구문으로 예외를 처리하는 프로그램
+		//패턴 1. main 함수에서 직접 try-catch 구문으로 예외를 처리하는 프로그램
 		try {
 			if (num2 == 0) throw 999;
 			cout << "rufrhk = " << num1 / num2 << "\n";
